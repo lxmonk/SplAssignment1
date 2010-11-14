6ebe6d71fe80f54660aa368f56baa602b2c28f2e
Aviad Reich <avi.rei@gmail.com>
2010-11-14 10:36:47 +0200

    starting to implement Manager/ManagerBoard

--- src/passiveobjects/ManagerBoardImpl.java
+++ src/passiveobjects/ManagerBoardImpl.java
@@ -3,34 +3,49 @@
  */
 package passiveobjects;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author lxmonk
- *
+ * 
  */
 public class ManagerBoardImpl implements ManagerBoard {
 
-	/* (non-Javadoc)
+	final Map<ManagerSpecializtion, ProjectBox> projectsMap;
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see passiveObjects.ManagerBoard#getPendingProjects()
 	 */
 	@Override
 	public Map<ManagerSpecializtion, ProjectBox> getPendingProjects() {
-		// TODO Auto-generated method stub
-		return null;
+		return new HashMap<ManagerSpecializtion, ProjectBox>(projectsMap);
 	}
 
-	/* (non-Javadoc)
-	 * @see passiveObjects.ManagerBoard#getProjectBox(passiveObjects.ManagerSpecializtion)
+	/*
+	 * private Map deepCopy(Map<ManagerSpecializtion, ProjectBox> map) {
+	 * Map<ManagerSpecializtion, ProjectBox> ret = new
+	 * HashMap<ManagerSpecializtion, ProjectBox>(); ret.putAll(map);
+	 * 
+	 * return ret; }
+	 */
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * passiveObjects.ManagerBoard#getProjectBox(passiveObjects.ManagerSpecializtion
+	 * )
 	 */
 	@Override
 	public ProjectBox getProjectBox(ManagerSpecializtion specializtion) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.projectsMap.get(specializtion);
 	}
-	
-	public void createProjectsMap(Map<ManagerSpecializtion, ProjectBox> map){
-		//TODO implement!
+
+	public void createProjectsMap(Map<ManagerSpecializtion, ProjectBox> map) {
+		// TODO implement!
 	}
 
 }
