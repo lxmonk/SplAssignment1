c6cea7816b03ec8a2be6acd33768de19b8049ea6
Aviad Reich <avi.rei@gmail.com>
2010-11-20 17:50:53 +0200

    date format fixed

--- src/main/OurFormatter.java
+++ src/main/OurFormatter.java
@@ -9,16 +9,18 @@ import java.util.logging.SimpleFormatter;
 
 /**
  * @author lxmonk
- *
+ * 
  */
 public class OurFormatter extends SimpleFormatter {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
 	 */
 	@Override
 	public String format(LogRecord record) {
-		return this.formatMessage(record)+"\n";
+		return this.formatMessage(record) + "\n";
 	}
 
 }
--- src/passiveobjects/Helpers.java
+++ src/passiveobjects/Helpers.java
@@ -11,21 +11,32 @@ import java.util.Date;
  */
 public class Helpers {
 
+	private static final int TEN = 10;
+
 	/**
 	 * A static method returning a string of the current time, in HH:MM:SS
 	 * format.
 	 * 
 	 * @return a string of the current time, in HH:MM:SS format.
 	 */
+	@SuppressWarnings("deprecation")
 	public static String staticTimeNow() {
 		Date date = new Date();
-		return date.getHours() + ":" + date.getMinutes() + ":"
-				+ date.getSeconds();
+		String hr, min, sec;
+		int hours = date.getHours(), minutes = date.getMinutes(), seconds = date
+				.getSeconds();
+		hr = String.valueOf(hours);
+		if (hours < Helpers.TEN) hr = "0" + hr;
+		min = String.valueOf(minutes);
+		if (minutes < Helpers.TEN) min = "0" + min;
+		sec = String.valueOf(seconds);
+		if (seconds < Helpers.TEN) sec = "0" + sec;
+
+		return hr + ":" + min + ":" + sec;
 	}
 
 	/**
-	 * A method returning a string of the current time, in HH:MM:SS
-	 * format.
+	 * A method returning a string of the current time, in HH:MM:SS format.
 	 * 
 	 * @return a string of the current time, in HH:MM:SS format.
 	 */
